fd9dbab176500d336cb497508090719873520a76
Chintu Kishen KM <me.chintukm@gmail.com>
2018-01-14 21:41:31 +0530

    elementary-electron

---
+++ index.js
@@ -0,0 +1,22 @@
+const picture = require('cat-picture');
+const image = require('lightning-image-poly');
+const {remote} = require('electron');
+const fs = require('fs');
+
+const src = picture.src
+picture.remove();
+
+const viz = new image('#visualization', null, [src], {hullAlgorithm: 'convex'});
+
+function save () {
+    remote.getCurrentWebContents().printToPDF(
+        {
+            portrait: true
+        },
+        function (err, data) {
+            fs.writeFile('annotation.pdf', data, function (err) {
+                if (err) alert('error generating pdf! ' + err.message)
+                else alert('pdf saved!')
+            })
+        })
+}
